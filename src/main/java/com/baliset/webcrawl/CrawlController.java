827fe636e2b236a8d45532b969e3d4d091e0d087
Haim Zamir <hzamir@gmail.com>
2017-09-01 14:33:35 -0400

    writes to file

--- src/main/java/com/baliset/webcrawl/CrawlController.java
+++ src/main/java/com/baliset/webcrawl/CrawlController.java
@@ -1,6 +1,7 @@
 package com.baliset.webcrawl;
 
 import de.felixroske.jfxsupport.*;
+import javafx.concurrent.*;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 import org.springframework.beans.factory.annotation.*;
@@ -11,30 +12,48 @@ public class CrawlController
   private CrawlConfig config;
   private EnumsConfig enumsConfig;
 
-  @Autowired public CrawlController(CrawlConfig config, EnumsConfig enumsConfig)
+  @Autowired
+  public CrawlController(CrawlConfig config, EnumsConfig enumsConfig)
   {
     this.config = config;
     this.enumsConfig = enumsConfig;
   }
 
-  @FXML private TextField initialUrl;
-  @FXML private TextField initialDomain;
-  @FXML private ChoiceBox<String> outputFormat;      // xml, yaml, or json
+  @FXML
+  private TextField initialUrl;
+  @FXML
+  private TextField initialDomain;
+  @FXML
+  private ChoiceBox<String> outputFormat;      // xml, yaml, or json
 
-  @FXML private CheckBox stayInDomain;     // stay in domain (don't go to another domain)
-  @FXML private CheckBox allowSubdomains;  // if inside domain are subdomains ok?
-  @FXML private Slider     minutesLimit;     // max time
-  @FXML private Slider     depthLimit;       // max depth
+  @FXML
+  private CheckBox stayInDomain;     // stay in domain (don't go to another domain)
+  @FXML
+  private CheckBox allowSubdomains;  // if inside domain are subdomains ok?
+  @FXML
+  private Slider minutesLimit;     // max time
+  @FXML
+  private Slider depthLimit;       // max depth
 
-  @FXML private ComboBox<String> userAgent;      // xml, yaml, or json
+  @FXML
+  private ComboBox<String> userAgent;      // xml, yaml, or json
 
 
-  @FXML private TextField minutesText;
-  @FXML private TextField depthText;
+  @FXML
+  private TextField minutesText;
+  @FXML
+  private TextField depthText;
 
-  @FXML private Button run;
+  @FXML
+  private Button run;
+
+  @FXML
+  private Label progressLabel;
+  @FXML
+  private ProgressBar progressBar;
 
   private Crawl worker;
+  private Task mytask;
 
   private void populate()
   {
@@ -47,21 +66,23 @@ public class CrawlController
     outputFormat.getItems().addAll(enumsConfig.getOutputFormats());
 
     outputFormat.setValue(config.getOutputFormat());
-    int mins =    config.getMinutesLimit();
+    int mins = config.getMinutesLimit();
     minutesLimit.valueProperty().set(mins);
-    minutesText.textProperty().set(""+mins);
+    minutesText.textProperty().set("" + mins);
 
     depthLimit.valueProperty().set(config.getDepthLimit());
-    depthText.textProperty().set(""+config.getDepthLimit());
+    depthText.textProperty().set("" + config.getDepthLimit());
 
     minutesLimit.setMin(1);
     minutesLimit.setMax(enumsConfig.getMaxMinutes());
 
     depthLimit.setMin(1);
     depthLimit.setMax(enumsConfig.getMaxDepth());
-    
+
     userAgent.getItems().addAll(enumsConfig.getUseragents());
     userAgent.setValue(config.getUseragent());
+
+    progressBar.setProgress(0.5);
   }
 
 
@@ -69,6 +90,52 @@ public class CrawlController
   {
     System.out.println(config.toString());
   }
+
+  private void crawlStart()
+  {
+    run.textProperty().set("Stop");
+    worker = new Crawl(config);
+    Thread th = new Thread(worker::crawl, "CrawlThread");
+    th.start();
+
+    progressBar.progressProperty().unbind();
+    progressBar.setProgress(0);
+    progressLabel.setVisible(true);
+    progressBar.setVisible(true);
+
+    mytask =  ProgressUtil.createProgressTask(100, config.getMinutesLimit()*60_000);
+    progressBar.progressProperty().bind(mytask.progressProperty());
+
+    mytask.messageProperty().addListener((observable, oldValue, newValue) -> {
+      if(!th.isAlive()) {
+        System.out.println("donem cancelling");
+        mytask.cancel();
+        progressBar.progressProperty().unbind();
+        progressBar.setProgress(1.0);
+        crawlCompleted();
+      }
+    });
+
+    new Thread(mytask).start();
+
+  }
+
+  private void crawlCompleted()
+  {
+    progressBar.progressProperty().unbind();
+//    progressLabel.setVisible(false);
+//    progressBar.setVisible(false);
+    run.textProperty().set("Start");
+  }
+
+  private void crawlStop()
+  {
+    crawlCompleted();
+    worker.stop();
+    worker = null;
+  }
+
+
   private void bindings()
   {
     initialUrl.textProperty().addListener((observable, ov, v) -> {
@@ -100,13 +167,9 @@ public class CrawlController
 
     run.setOnAction((event) -> {
       if(worker == null) {
-        run.textProperty().set("Stop");
-        worker = new Crawl(config);
-        new Thread(worker::crawl, "CrawlThread").start();
+        crawlStart();
       } else {
-        worker.stop();
-        worker = null;
-        run.textProperty().set("Start");
+        crawlStop();
       }
     });
   }
